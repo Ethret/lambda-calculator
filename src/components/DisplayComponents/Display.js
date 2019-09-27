a2f16d3d42106620055d48e2ef2ad56dbc68852a
Ethret <Ethret@users.noreply.github.com>
2019-09-27 11:27:08 -0500

    did a whole buncha stuff. mvp completed.

--- src/components/DisplayComponents/Display.js
+++ src/components/DisplayComponents/Display.js
@@ -1,5 +1,10 @@
+
 import React from "react";
 
 const Display = () => {
-  return <div className="display">{/* Display any props data here */}</div>;
+  return <div className="display">
+    <div>{0}</div>
+  </div>;
 };
+
+export default Display;
